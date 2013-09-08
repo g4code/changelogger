9168a534dde574b5023a2bff9b7b7d5cea8b3a43
Ates <drasko.gomboc@gmail.com>
2013-09-08 18:54:46 +0200

    app fullPath test; not a git repo message

---
+++ test/lib/app.js
@@ -0,0 +1,28 @@
+
+var should = require('chai').should(),
+	App    = require("../../lib/app");
+	path   = require("path");
+
+describe('App', function(){
+	
+	var app = new App();
+	
+	beforeEach(function(){
+	
+		app.fullPath = null;
+	});
+	
+	describe('getFullPath', function(){
+
+		it('should return current dir full path', function(){
+	    	
+	    	app.setArgs([]).getFullPath().should.be.equal(path.resolve("./"));
+	    });
+	    
+	    it('should return parent dir full path', function(){
+	    	
+	    	app.setArgs(["../", ""]).getFullPath().should.be.equal(path.resolve("../"));
+	    });
+	});
+	
+});
