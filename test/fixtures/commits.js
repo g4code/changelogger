181fc158495043fee73386c57f9dfb754fefe108
Drasko Gomboc <drasko.gomboc@gmail.com>
2013-09-16 14:58:53 +0200

    git mediator object; app object refactored

---
+++ test/fixtures/commits.js
@@ -0,0 +1,17 @@
+
+var commits = [{
+                    abbreviatedHash: 'e344135',
+                    hash: 'e34413548f5c989e65d7155212fe81f07bbcbce5',
+                    authorName: 'Drasko Gomboc',
+                    authorDate: '2013-08-27 17:02:10 +0200',
+                    subject: 'npm package config added'
+                },
+                {
+                    abbreviatedHash: '0d1e5f1',
+                    hash: '0d1e5f1b3277e08a724db40ce35aa4d755d6e3c8',
+                    authorName: 'Drasko Gomboc',
+                    authorDate: '2013-08-27 16:36:13 +0200',
+                    subject: 'makefile added'
+                }];
+
+module.exports = commits;
--- test/lib/git.js
+++ test/lib/git.js
@@ -1,6 +1,7 @@
 
-var should = require("chai").should(),
-    Git    = require("../../lib/git");
+var should  = require("chai").should(),
+    Git     = require("../../lib/git"),
+    commits = require("../fixtures/commits");
 
 describe("Git", function(){
 
@@ -9,7 +10,7 @@ describe("Git", function(){
         it("should return true if current folder is git repo", function(done){
 
             var git = new Git();
-            git.setFullPath("./")
+            git.setRepoPath("./")
                .status(function(isGitRepo){
                    isGitRepo.should.be.true;
                    done();
@@ -19,10 +20,59 @@ describe("Git", function(){
 
     describe("onStatus", function(){
 
-        if("should call log method", function(){
+        it("should call log method", function(){
 
             var git = new Git();
+            git.log = function(){
+                "test".should.be.ok;
+            };
+            git.onStatus(true);
+        });
 
+        it("should call notGitRepo method", function(){
+
+            var git = new Git();
+            git.notGitRepo = function(){
+                "test".should.be.ok;
+            };
+            git.onStatus(false);
+        });
+    });
+
+    describe("log", function(){
+
+        it("should return commit array", function(done){
+
+            var git = new Git();
+            git.setRepoPath("./")
+               .log(function(commits){
+                   commits.should.be.an("array");
+                   commits[0].should.be.an("object");
+                   done();
+               });
+        });
+    });
+
+    describe("onLog", function(){
+
+        it("should call noCommits method", function(done){
+
+            var git = new Git();
+            git.noCommits = function(){
+                "test".should.be.ok;
+                done();
+            };
+            git.onLog([]);
+        });
+
+        it("should call tag method", function(done){
+
+            var git = new Git();
+            git.tag = function(){
+                "test".should.be.ok;
+                done();
+            };
+            git.onLog(commits);
         });
     });
 
