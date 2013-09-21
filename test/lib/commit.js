c1aebf121e6672438c5b1cd2f2c9f432d4f626ea
Ates <drasko.gomboc@gmail.com>
2013-09-21 20:03:56 +0200

    commit object and test

---
+++ test/lib/commit.js
@@ -0,0 +1,15 @@
+
+var should = require("chai").should(),
+    Commit = require("../../lib/commit");
+
+describe("Commit", function(){
+
+    describe("init", function(){
+
+        it("should find commits if there is no tags", function(){
+
+            var commit = new Commit();
+
+        });
+    });
+});
